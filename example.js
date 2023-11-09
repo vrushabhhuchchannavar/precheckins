7121c789b854d2745c8197ceb4457463af2e04f5
Vrushabh Huchchannavar <vrushabh.huchchannavar@ecobillz.com>
2023-11-09 15:17:28 +0530

    initial cummit

---
+++ example.js
@@ -0,0 +1,10 @@
+// let a = 2;
+// let b = 2;
+
+const value = (a, b) => {
+    return a + b;
+}
+
+// let s = value();
+// console.log(s)
+module.exports = value;
---
+++ value.test.js
@@ -0,0 +1,21 @@
+const val = require('./example');
+const request = require('supertest');
+
+
+
+    it('sum of a,b', () => {
+        const result = val(2,2);
+        expect(result).toBe(4);
+    });
+
+    it('sum is ', () => {
+        const result = val(2,3);
+        expect(result).toBe(5);
+    });
+
+    it('fail', () => {
+        const result = val(2,3);
+        expect(result).toBe();
+    });
+
+
